5ee57acd9716d5ee57952304b3351612d92d566a
legendarynhico@gmail.com <legendarynhico@gmail.com>
2025-12-10 10:03:52 +0800

    Create rentproject.py

---
+++ rentproject.py
@@ -0,0 +1,6 @@
+rent =int(input("Enter the rent amount: "))
+months =int(input("Enter the number of months: "))
+total_rent = rent * months
+print("The total rent for", months, "months is:", total_rent)
+
+# This is a simple rent calculator that multiplies the monthly rent by the number of months to get the total rent.
